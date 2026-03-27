a349ff4abdd5ab97d1548f3fd33e42deb2505e15
Mohammadreza Mesgari <mmesgari@f4r2s5.clusters.42paris.fr>
2026-03-27 13:36:36 +0100

    My own version

--- Level_2/alpha_mirror/alpha_mirror.c
+++ Level_2/alpha_mirror/alpha_mirror.c
@@ -3,38 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   alpha_mirror.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mmesgari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 19:25:18 by alex              #+#    #+#             */
-/*   Updated: 2024/04/11 19:57:48 by alex             ###   ########.fr       */
+/*   Created: 2026/03/27 09:48:37 by mmesgari          #+#    #+#             */
+/*   Updated: 2026/03/27 12:55:26 by mmesgari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	alpha_mirror(char *str)
-{
-	char	mirror;
-	int		i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			mirror = 'z' - (str[i] - 'a');
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			mirror = 'Z' - (str[i] - 'A');
-		else
-			mirror = str[i];
-		write(1, &mirror, 1);
-		i++;
-	}
-}
-
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
-		alpha_mirror(argv[1]);
-	write(1, "\n", 1);
-	return (0);
+		int i = 0;
+
+		if (argc == 2)
+		{
+				while (argv[1][i])
+				{
+						if ((argv[1][i] >= 'a' && argv[1][i] <= 'z'))
+								argv[1][i] = ('z' + 'a') - argv[1][i];
+						if ((argv[1][i] >= 'A' && argv[1][i] <= 'Z'))
+								argv[1][i] = ('Z' + 'A') - argv[1][i];
+						write (1, &argv[1][i], 1);
+						i++;
+				}
+
+		}
+		write(1, "\n", 1);
+		return (0);
 }
